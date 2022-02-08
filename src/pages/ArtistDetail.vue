baf66d6ded0003b64c5c8189cfddb78a5939a63c
guozhao <davidkoojohn@163.com>
2022-02-08 18:20:47 +0800

    add detail info

--- src/pages/ArtistDetail.vue
+++ src/pages/ArtistDetail.vue
@@ -6,6 +6,17 @@
     <p>专辑数：{{ artistInfo.albumSize }}</p>
     <p>mv数：{{ artistInfo.mvSize }}</p>
     <p class="desc">简介：{{ artistInfo.briefDesc }}</p>
+    <ol>
+      <li v-for="item of artistSong50" :key="item.id">{{ item.name }}</li>
+    </ol>
+    <hr>
+    <ol>
+      <li v-for="item of artistMV" :key="item.id" @click="playMV(item.id)">
+        <img :src="item.imgurl" width="200" alt="">
+        <p>{{ item.name }}</p>
+      </li>
+    </ol>
+    <video controls :src="mp4" width="400" autoplay></video>
   </base-layout>
 </template>
 
@@ -15,16 +26,39 @@ import { useRoute } from "vue-router"
 
 const route = useRoute()
 const { id } = route.params
-console.log(id)
+
 const artistInfo = ref<any>({})
+const artistSong50 = ref<any>({})
+const artistMV = ref<any>({})
+
 const getArtistDetail = async () => {
   const { data } = await fetch(`http://localhost:3000/artist/detail?id=${id}`).then(res => res.json())
-  console.log(data.artist)
+  // console.log(data.artist)
   artistInfo.value = data.artist
+}
+
+const getArtist50 = async () => {
+  const { songs } = await fetch(`http://localhost:3000/artist/top/song?id=${id}`).then(res => res.json())
+  artistSong50.value = songs
+}
+
+const getArtistMV = async () => {
+  const { mvs } = await fetch(`http://localhost:3000/artist/mv?id=${id}`).then(res => res.json())
+  artistMV.value = mvs
+  console.log(mvs)
+}
+
+const mp4 = ref("")
+const playMV = async (id: number) => {
+  const { data } = await fetch(`http://localhost:3000/mv/url?id=${id}&r=1080`).then(res => res.json())
+  console.log(data.url)
+  mp4.value = data.url
 }
 
 onBeforeMount(async () => {
   await getArtistDetail()
+  await getArtist50()
+  await getArtistMV()
 })
 </script>
 
