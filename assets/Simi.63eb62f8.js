cf5978ac8315c132f1003bd08bf64a46e71c99dc
davidkoojohn <davidkoojohn@gmail.com>
2022-02-15 10:25:15 +0000

    Deploying to gh-pages from  @ 969f0595d273f6023d8d85b5c0ed25ebbd4de679 🚀

--- assets/Simi.63eb62f8.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.4e7ce97c.js";import{o as r,z as o}from"./vendor.62ae568e.js";const c={};function t(n,a){return r(),o("h1",null,"simi")}var i=e(c,[["render",t]]);export{i as default};
